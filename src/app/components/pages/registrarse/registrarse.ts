91be4df964bda23c3346efdfd676cca1ce3c92b5
Charles <charlesomargp@gmail.com>
2025-12-01 10:38:53 +0100

    actu

--- src/app/components/pages/registrarse/registrarse.ts
+++ src/app/components/pages/registrarse/registrarse.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './registrarse.html',
   styleUrls: ['./registrarse.css']
 })
-export class RegistrarseComponent {
+export class Registrarse {
   usuario = {
     nombre: '',
     apellido: '',
