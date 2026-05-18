8e0a1cdd6c8877926cd472ad628310de5950e6d7
Charles <charlesomargp@gmail.com>
2026-05-18 22:06:00 +0200

    upload ra4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,48 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router } from '@angular/router';
 import { Cabecera } from "./components/cabecera/cabecera";
 import { Pie } from "./components/pie/pie";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, Cabecera, Pie, RouterModule],
+    imports: [RouterOutlet, Cabecera, Pie, RouterModule, FormsModule, CommonModule],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
 export class App {
-    protected readonly title = signal('panaderia');
+    textoBuscar: string = '';
+
+    productosSugeridos = [
+        { nombre: 'Pan de Pueblo', link: '/pueblo' },        
+        { nombre: 'Pan de Leña', link: '/lenia' },          
+        { nombre: 'Pan Blanco', link: '/blanco' },          
+        { nombre: 'Pan Integral', link: '/integral' },     
+        { nombre: 'Bizcocho de Vainilla', link: '/bizcocho' }, 
+        { nombre: 'Croissant Clásico', link: '/croissant' },   
+        { nombre: 'Galletas de Chocolate', link: '/galletas' },
+        { nombre: 'Magdalenas Caseras', link: '/magdalenas' }
+    ];
+
+    constructor(private router: Router) {}
+
+    //  BUSCADOR  de productos
+    irAPagina(url: string) {
+        this.router.navigate([url]);
+        this.textoBuscar = ''; // Limpia el buscador después de navegar
+    }
+
+    mostrarBuscador(): boolean {
+        const ruta = this.router.url;
+        return !(ruta.includes('inicio-sesion') || ruta.includes('registrarse'));
+    }
+
+    get sugerenciasFiltradas() {
+        if (this.textoBuscar.length < 2) return [];
+        return this.productosSugeridos.filter(p => 
+            p.nombre.toLowerCase().includes(this.textoBuscar.toLowerCase())
+        );
+    }
 }
--- src/app/components/pages/pastelito/pastelito.ts
+++ src/app/components/pages/pastelito/pastelito.ts
@@ -1,26 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-/**
- * Componente encargado de mostrar la vista de "Pastelito".
- * Actualmente no contiene lógica interna, pero sirve como punto de navegación
- * dentro de la aplicación mediante el uso de RouterLink.
- *
- * @component
- */
 @Component({
   selector: 'app-pastelito',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [ CommonModule, FormsModule],
   templateUrl: './pastelito.html',
-  styleUrls: ['./pastelito.css'], // Corregido styleUrl -> styleUrls
+  styleUrls: ['./pastelito.css'],
 })
 export class Pastelito {
+  textoBuscar: string = '';
+  mostrarTranscripcion: boolean = false;
 
-  mostrarCta: boolean = true; 
+  
+  todosLosProductos = [
+    { id: 13, nombre: 'Bizcocho ', categoria: 'Pastelería', precio: 5.50, img: 'https://...' },
+    { id: 16, nombre: 'Croissant ', categoria: 'Pastelería', precio: 2.50, img: 'https://...' },
+    { id: 19, nombre: 'Galletas ', categoria: 'Pastelería', precio: 3.50, img: 'https://...' },
+    { id: 1, nombre: 'Pan de Pueblo', categoria: 'Panes', precio: 1.20, img: 'https://www.recetasderechupete.com/wp-content/uploads/2020/04/Pan-de-pueblo.jpg' }
+   
+  ];
+  
+  // Función que devuelve los productos filtrados en tiempo real
+  get productosFiltrados() {
+    return this.todosLosProductos.filter(p => 
+      p.nombre.toLowerCase().includes(this.textoBuscar.toLowerCase())
+    );
+  }
 
-  /**
-   * Constructor del componente Pastelito.
-   * No recibe dependencias ya que el componente actúa solamente como vista.
-   */
-  constructor() {}
+  anadirProducto(id: number, nombre: string, precio: number) {
+    console.log(`Añadido al carrito: ${nombre}`);
+  }
 }
